c24731299761b0cf6a17c41b8fdb26bfa918392a
Madhu Krishna <madhukrishna2004@gmail.com>
2025-11-19 15:22:49 +0530

    Miyraa deploy

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -5,6 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    open: false
-  }
+  },
+  build: {
+    outDir: "dist",
+  },
 });
